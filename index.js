be52b72365e0f2abe61170c851bd3e681245f00b
A.Rupesh <a.rupesh66@gmail.com>
2018-10-11 02:59:51 +0530

    browserWindow created

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+  new BrowserWindow({
+      height: 600,
+      width: 800,
+      webPreferences: { backgroundThrottling: false }
+  });
+});
