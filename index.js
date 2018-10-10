30144c9ba21dd12d079b446ef27bcb9eb1b6e820
A.Rupesh <a.rupesh66@gmail.com>
2018-10-11 03:06:13 +0530

    electron startup with webpack

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const electron = require('electron');
 
 const { app, BrowserWindow } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
   new BrowserWindow({
       height: 600,
       width: 800,
       webPreferences: { backgroundThrottling: false }
   });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
