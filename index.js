c98046935fd6c7d4236d884f7cb18cf5a2950e19
A.Rupesh <a.rupesh66@gmail.com>
2018-10-12 01:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ app.on('ready', () => {
 });
 
 ipcMain.on('VIDEOS_ADDED', (event, videos) => {
-    const promise = new Promise((resolve, reject) => {
-        let ffmpeg = fluent_ffmpeg(videos[0].path).setFfprobePath('C:\\Users\\Ben\\ffmpeg\\bin\\ffprobe.exe');
-        ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
-            resolve(metadata);
-        });
-    });
+//     const promise = new Promise((resolve, reject) => {
+//         let ffmpeg = fluent_ffmpeg(videos[0].path).setFfprobePath('C:\\Users\\Ben\\ffmpeg\\bin\\ffprobe.exe');
+//         ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+//             resolve(metadata);
+//         });
+//     });
 
-    promise.then((metadata) => {console.log(metadata);})
-});
+//     promise.then((metadata) => {console.log(metadata);})
