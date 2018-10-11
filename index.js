8c93ab8c08c52be08a0b0251df362b5f908ac182
A.Rupesh <a.rupesh66@gmail.com>
2018-10-12 02:26:50 +0530

    final fixes

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 const electron = require('electron');
-const {app, BrowserWindow, ipcMain, shell} = electron;
-const fluent_ffmpeg = require('fluent-ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
 const _ = require('lodash');
+
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-  new BrowserWindow({
-      height: 600,
-      width: 800,
-      webPreferences: { backgroundThrottling: false }
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: { backgroundThrottling: false }
   });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
-ipcMain.on('VIDEOS_ADDED', (event, videos) => {
 
-
-    const promises = _.map(videos, video => {
-      // promise.then((metadata) => {console.log(metadata);})
-        return new Promise((resolve, reject) => {
-              ffmpeg.ffprobe(video.path, (err, metadata) => {
-                resolve(video);
-            });
-        });
-
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
       });
-    Promise.all(promises)
-    .then((results) => console.log(results));
+    });
   });
 
+  Promise.all(promises)
+    .then((results) => {
+      mainWindow.webContents.send('metadata:complete', results);
+    });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name)[0];
+    const outputName = video.name.split('.')[0]
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('progress', ({ timemark }) =>
+        mainWindow.webContents.send('conversion:progress', { video, timemark })
+      )
+      .on('end', () =>
+        mainWindow.webContents.send('conversion:end', { video, outputPath })
+      )
+      .run();
+  });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
+});
--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,9 @@ import { ADD_VIDEO, ADD_VIDEOS, REMOVE_VIDEO, REMOVE_ALL_VIDEOS, VIDEO_PROGRESS,
 // TODO: Communicate to MainWindow process that videos
 // have been added and are pending conversion
 export const addVideos = videos => dispatch => {
-    ipcRenderer.send('VIDEOS_ADDED', videos);
-  ipcRenderer.on('METADATA_COMPLETE', (event, videosWithData) => {
-      dispatch({type: ADD_VIDEOS, payload: videosWithData});
+  ipcRenderer.send('videos:added', videos);
+  ipcRenderer.on('metadata:complete', (event, videosWithData) => {
+    dispatch({ type: ADD_VIDEOS, payload: videosWithData });
   });
 };
 
@@ -14,14 +14,22 @@ export const addVideos = videos => dispatch => {
 // to start converting videos.  Also listen for feedback
 // from the MainWindow regarding the current state of
 // conversion.
-export const convertVideos = () => (dispatch, getState) => {
+export const convertVideos = videos => dispatch => {
+  ipcRenderer.send('conversion:start', videos);
 
+  ipcRenderer.on('conversion:end', (event, { video, outputPath }) => {
+    dispatch({ type: VIDEO_COMPLETE, payload: { ...video, outputPath } });
+  });
+
+  ipcRenderer.on('conversion:progress', (event, { video, timemark }) => {
+    dispatch({ type: VIDEO_PROGRESS, payload: { ...video, timemark }});
+  });
 };
 
 // TODO: Open the folder that the newly created video
 // exists in
 export const showInFolder = outputPath => dispatch => {
-
+  ipcRenderer.send('folder:open', outputPath);
 };
 
 export const addVideo = video => {
